f1b004a1d7acc782d63d034f97bc263c99b389a2
Keith Moss <keithamoss@gmail.com>
2018-08-17 16:24:04 +0800

    WIP

---
+++ literary-screensaver/ConfigureSheetController.swift
@@ -0,0 +1,39 @@
+//
+//  ConfigureSheetController.swift
+//  ScreenSaverMinimal
+//
+//  Created by Mirko Fetter on 28.10.16.
+//
+//  Based on https://github.com/erikdoe/swift-circle
+
+
+import Cocoa
+
+class ConfigureSheetController : NSObject {
+    
+    var defaultsManager = DefaultsManager()
+
+    @IBOutlet var window: NSWindow?
+    @IBOutlet var canvasColorWell: NSColorWell?
+
+
+    override init() {
+        super.init()
+        let myBundle = Bundle(for: ConfigureSheetController.self)
+        myBundle.loadNibNamed("ConfigureSheet", owner: self, topLevelObjects: nil)
+        canvasColorWell!.color = defaultsManager.canvasColor
+    }
+
+    @IBAction func updateDefaults(_ sender: AnyObject) {
+        defaultsManager.canvasColor = canvasColorWell!.color
+    }
+   
+    @IBAction func closeConfigureSheet(_ sender: AnyObject) {
+        NSColorPanel.shared().close()
+        
+        // Deprecated in macOS 10.10
+//        NSApp.endSheet(window!)
+        window!.endSheet(window!)
+    }
+
+}
